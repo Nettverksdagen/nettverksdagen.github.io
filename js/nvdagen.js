d4a84a8d798d80fafe18b0c0576df244dba7e940
Space <space@471.no>
2016-09-16 21:32:52 +0200

    Adding row max widths

--- js/nvdagen.js
+++ js/nvdagen.js
@@ -10,4 +10,27 @@ function DayDiff(CurrentDate, compareDate) {
 $(document).ready(function() {
 	var Today = new Date();
 	document.querySelector(".timeLeft").innerHTML = " - " + DayDiff(Today, "February, 2, 2017") + " dager igjen";
+	
+	$('.modal').modal('hide');
+	$('.toggleModal').click(function(ev){
+		ev.preventDefault();
+		console.log("toggle modal executed");
+		if (!!document.querySelector('body.modal-open')) {
+			$('.modal').modal('hide');
+		} else {
+			console.log(this);
+			var pid = $(this).data('modal');
+			$.get('/modals/' + pid + '.txt', function(data){
+				var headerRegex = /(?:<header>)(.|[\r\n])*(?:<\/header>)/g;
+				var contentRegex = /(?:<content>)(.|[\r\n])*(?:<\/content>)/g;
+				var headerContent = headerRegex.exec(data)[0];
+				var bodyContent = contentRegex.exec(data)[0];
+				$('.modal .modal-title').html(headerContent);
+				$('.modal .modal-body').html(bodyContent);
+				$('.modal').modal('show');
+				document.querySelector('.modal').focus();
+				$(".modal-content").click();
+			});
+		}
+	});
 });
