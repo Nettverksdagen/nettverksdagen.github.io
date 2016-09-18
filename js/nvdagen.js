59f4a691c6421adfaef8feb1ee656dfffd48f064
Space <space@471.no>
2016-09-18 19:30:58 +0200

    Fixed nav links, rm lorem ipsum

--- js/nvdagen.js
+++ js/nvdagen.js
@@ -25,6 +25,7 @@ $(document).ready(function() {
 				var contentRegex = /(?:<content>)(.|[\r\n])*(?:<\/content>)/g;
 				var headerContent = headerRegex.exec(data)[0];
 				var bodyContent = contentRegex.exec(data)[0];
+				document.querySelector(".modal").style.marginTop = document.querySelector("#navbar").offsetHeight+"px";
 				$('.modal .modal-title').html(headerContent);
 				$('.modal .modal-body').html(bodyContent);
 				$('.modal').modal('show');
