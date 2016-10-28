367a17930156460c2888720028ec839cb3bb3613
Space <space@471.no>
2016-10-28 15:19:54 +0200

    Modal/navbar opening bug fixed, minor spelling correction

--- js/nvdagen.js
+++ js/nvdagen.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
 				var contentRegex = /(?:<content>)(.|[\r\n])*(?:<\/content>)/g;
 				var headerContent = headerRegex.exec(data)[0];
 				var bodyContent = contentRegex.exec(data)[0];
-				document.querySelector(".modal").style.marginTop = document.querySelector("#navbar").offsetHeight+"px";
+				document.querySelector(".modal").style.marginTop = document.querySelector(".navbar-header").offsetHeight+"px";
 				$('.modal .modal-title').html(headerContent);
 				$('.modal .modal-body').html(bodyContent);
 				$('.modal').modal('show');
